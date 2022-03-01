e06584c922986326fdd6541d3e3e7f7b019cce54
MonadWizard <monad.wizard.r@gmail.com>
2022-03-01 19:14:37 +0600

    0.08_user_profile_socialAbout

--- probashi_backend/user_profile_app/urls.py
+++ probashi_backend/user_profile_app/urls.py
@@ -6,7 +6,7 @@ from .views import UserProfileSkipPart1, UserProfileSkipPart2, UserAboutSocialLi
 urlpatterns = [
     path('user-profile-skip1/<str:userid>/', UserProfileSkipPart1.as_view(), name="UserProfileskip1"),
     path('user-profile-skip2/<str:userid>/', UserProfileSkipPart2.as_view(), name="UserProfileskip2"),
-    path('user-about-social-link/', UserAboutSocialLink.as_view(), name="UserAboutSocialLink"),
+    path('user-about-social-link/<str:userid>/', UserAboutSocialLink.as_view(), name="UserAboutSocialLink"),
 
 ]
 
