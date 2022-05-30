ad16e13dc216c5cdfdd118734efd774f4b49474f
MonadWizard <monad.wizard.r@gmail.com>
2022-05-30 08:58:46 +0600

    000.38 delete user remove from friend suggation

--- auth_user_app/signals.py
+++ auth_user_app/signals.py
@@ -1,11 +1,40 @@
-from django.db.models.signals import post_delete
+from django.db.models.signals import pre_delete
 from .models import User
 from django.dispatch import receiver
+from user_connection_app.models import FriendsSuggation
 
 
-@receiver(post_delete, sender=User)
+@receiver(pre_delete, sender=User)
 def post_delete_action(sender, **kwargs):
 
-    print("post_delete_action.................")
     user = kwargs["instance"]
-    print("kwargs:::::::::::::::", user.userid)
+
+    user_id = [str(user.userid)]
+    # print("user_id:", user_id)
+
+    fields = [
+        "location",
+        "goals",
+        "interest",
+        "durationyear_abroad",
+        "current_location_durationyear",
+        "industry",
+        "areaof_experience",
+        "industry_experienceyear",
+        "serviceholder",
+        "selfemployed",
+        "currentdesignation",
+        "company_name",
+        "office_address",
+    ]
+    for field in fields:
+        filterr = f"{field}__overlap"
+        dlt_field = FriendsSuggation.objects.filter(**{filterr: user_id}).values()
+        if dlt_field:
+            for u in dlt_field:
+                u[f"{field}"].remove(user_id[0])
+                terget_user = FriendsSuggation.objects.get(id=u["id"])
+                terget_user.__dict__[field] = u[f"{field}"]
+                terget_user.save()
+
+    FriendsSuggation.objects.filter(user=user).delete()
