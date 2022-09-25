1bdbe6b4015d8b148b3e0a25b8adfc6e36886b66
MonadWizard <monad.wizard.r@gmail.com>
2022-09-25 13:24:29 +0600

    apple complete

--- social_auth/views.py
+++ social_auth/views.py
@@ -66,7 +66,7 @@ class AppleSocialAuthView(GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = (serializer.validated_data)["auth_token"]
-        data["user_callphone"] = None
+        # data["user_callphone"] = None
 
         print(data)
         return Response(data, status=status.HTTP_200_OK)
